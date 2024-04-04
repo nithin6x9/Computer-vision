9166fe0d1134bcc871d6b8bdfbfad94949dcdd32
nithin-nk <nithinkrishnalal303@gmail.com>
2024-04-04 14:53:20 +0530

    example 1

--- lab-works/lab-5/example.py
+++ lab-works/lab-5/example.py
@@ -2,17 +2,15 @@ import numpy as np
 import cv2
 
 f_t = cv2.imread('lab-works/lab-5/assets/images.jpeg')
-f = f_t.astype(np.float64);
-sigma = 25;
-#g = f + np.random.randn(f.shape[0],f.shape[1])*sigma;
-g = f + np.random.randn(f.shape[0], f.shape[1]) * sigma
+f = f_t.astype(np.float64)
+sigma = 25
+noise = np.random.randn(*f.shape) * sigma  
+g = f + noise
 
-up_th = g > 255;
-lo_th = g < 0
-g[up_th] = 255 ; 
-g[lo_th] = 0;
+g[g > 255] = 255
+g[g < 0] = 0
 
-cv2.imshow(f_t);
-cv2.imshow(g)
+cv2.imshow('Original Image', f_t)
+cv2.imshow('Noisy Image', g.astype(np.uint8))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
